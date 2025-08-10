4a903c81edc1cd747e9fb9a391b53efe3ff2fc16
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2025-08-10 23:59:26 +0530

    commit

--- Java practise/patterns/first.java
+++ Java practise/patterns/first.java
@@ -26,8 +26,20 @@ public class first {
             System.out.println();
         }
     }
+
+    public static void inverted_half_pyramid_withNumbers(int n) {
+        for(int i=1; i<=n; i++) {
+            //inner - numbers
+            for(int j=1; j<=n-i+1;j++) {
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String args[]) {
         // hollow_rectangle(4, 5);
         inverted_rotated_half_pyramid(4);
+        inverted_half_pyramid_withNumbers(5);
     }
 }
