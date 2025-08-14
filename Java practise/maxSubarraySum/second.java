eb96eed697befc6197194181513703217e4efea9
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2025-08-14 21:12:37 +0530

    commit

---
+++ Java practise/maxSubarraySum/second.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class second {
+    public static int maxSubarraySum(int[] numbers){
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+        int currSum = 0;
+
+        prefix[0] = numbers[0];
+        for(int i=1;i<prefix.length;i++) {
+            prefix[i]=prefix[i-1] + numbers[i];
+        }
+
+        for(int i=0;i<numbers.length;i++) {
+            int start = i;
+            for(int j=i;j<numbers.length;j++) {
+                int end = j;
+
+                currSum = start == 0 ? prefix[end] : prefix[end]-prefix[start];
+                if( currSum>maxSum){
+                    maxSum = currSum;
+                }
+
+            }
+        }
+        System.out.println("max sum = " + maxSum);
+        return maxSum;
+    }
+    public static void main(String args[]){
+        int[] numbers = {4, 6, 7, 5, 3};
+        maxSubarraySum(numbers); 
+    }
+}
