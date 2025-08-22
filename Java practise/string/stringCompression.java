d80b6ac1b4a40a76d7a35a99ba1a97bac33f1286
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2025-08-23 00:00:05 +0530

    commit

---
+++ Java practise/string/stringCompression.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class stringCompression {
+    
+    public static String compress(String str) {
+        String newStr = "";
+
+        for(int i=0; i<str.length(); i++) {
+            Integer count = 1;
+            while(i<str.length()-1 && str.charAt(i) == str.charAt(i+1)) {
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if(count>1) {
+                newStr += count.toString();
+            }
+        }
+        return newStr;
+    }
+    public static String compress1(String str){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<str.length();i++) {
+            int count = 1;
+
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)) {
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+
+            if(count>1) {
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String args[]){
+        String str = "aaabbbcccaaa";
+        String compressed = compress1(str);
+        System.out.println(compressed);
+    }
+}
